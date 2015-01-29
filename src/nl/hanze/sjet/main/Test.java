036c48acfe6baf1f57ebe0b3ab0b15c6332205ee
Serkan Orhan <ser66orh_97@live.nl>
2015-01-29 14:14:57 +0100

    Week 3 Versie 4 -knoppen werken -animal editing frame moet afgemaakt worden -JUnit moet ook af

---
+++ src/nl/hanze/sjet/main/Test.java
@@ -0,0 +1,25 @@
+package nl.hanze.sjet.main;
+import nl.hanze.sjet.model.Location;
+import nl.hanze.sjet.model.Simulator;
+
+import static org.junit.Assert.*;
+
+public class Test {
+	Location location;
+	
+	@org.junit.Test
+	public void test() {
+		Simulator simulator = new Simulator();
+		boolean test = false;
+		for(int i = 0; i < 1000; i++){
+			simulator.simulateOneStep();
+			test = true;
+		}
+		if(test == true){
+			assertEquals(simulator.getStep(),1000);
+			
+		}
+		
+	}
+
+}
